8647023a8bb2e2923d6d27cb0703c1c6868ec469
Aman Sharma <droj.co@gmail.com>
2023-07-26 08:14:39 +0530

    -
    -

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Typed from 'react-typed'
+// import Typed from 'react-typed'
 import "./AboutMe.css"
 const AboutMe = () => {
   return (
@@ -8,7 +8,7 @@ const AboutMe = () => {
         {/* <p className='p1'>SOFTWERE DEVELOPER</p>
         <p className='p2'>Hello I am Aman Sharma &#60;He,Him/&#62; </p> */}
         {/* <Typed className="h1" strings={["the site is under developement"]} */}
-        typeSpeed={70} />
+        {/* typeSpeed={70} /> */}
         
     </div>
   )
