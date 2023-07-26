67838d9de547f6946f2a2fa03658ac9ff5372839
Aman Sharma <droj.co@gmail.com>
2023-07-26 08:09:53 +0530

    -
    -

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,9 +4,10 @@ import "./AboutMe.css"
 const AboutMe = () => {
   return (
     <div className='about-div'>
+      <h1>under developement</h1>
         {/* <p className='p1'>SOFTWERE DEVELOPER</p>
         <p className='p2'>Hello I am Aman Sharma &#60;He,Him/&#62; </p> */}
-        <Typed className="h1" strings={["the site is under developement"]}
+        {/* <Typed className="h1" strings={["the site is under developement"]} */}
         typeSpeed={70} />
         
     </div>
